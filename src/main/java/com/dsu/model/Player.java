53ffdc2ed1543ba3386cb24c80628b8ce475bc3e
Jesus Barrera <jesus.barrera@telusinternational.com>
2022-08-19 10:53:07 -0600

    Added CRUD funcionality to Player class

--- src/main/java/com/dsu/model/Player.java
+++ src/main/java/com/dsu/model/Player.java
@@ -1,26 +1,70 @@
+package com.dsu.model;
+
+import java.util.List;
+
 public class Player {
     private static List<Player> players;
     private int idPlayer;
     private String name;
     private int idTypePlayer;
 
-    public static insertPlayer(Player player) {
-
+    public int getIdPlayer() {
+        return idPlayer;
     }
 
-    public static selectPlayer(int id) {
-        
+    public void setIdPlayer(int idPlayer) {
+        this.idPlayer = idPlayer;
     }
 
-    public static selectPlayers() {
-        
+    public String getName() {
+        return name;
     }
 
-    public static updatePlayer(int id, Player player) {
-        
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public static deletePlayer(Player player) {
-        
+    public int getIdTypePlayer() {
+        return idTypePlayer;
+    }
+
+    public void setIdTypePlayer(int idTypePlayer) {
+        this.idTypePlayer = idTypePlayer;
+    }
+
+    public static Player insertPlayer(Player player) {
+        players.add(player);
+        return player;
+    }
+
+    public static Player selectPlayer(int id) {
+        for (Player player : players)
+            if (player.getIdPlayer() == id)
+                return player;
+        return null;
+    }
+
+    public static List<Player> selectPlayers() {
+        return players;
+    }
+
+    public static Player updatePlayer(int id, Player player) {
+        for (Player foundPlayer : players) {
+            if (foundPlayer.getIdPlayer() == id) {
+                foundPlayer.setIdPlayer(player.getIdPlayer());
+                foundPlayer.setName(player.getName());
+                foundPlayer.setIdTypePlayer(player.getIdTypePlayer());
+                return foundPlayer;
+            }
+        }
+        players.add(player);
+        return player;
+    }
+
+    public static void deletePlayer(Player player) {
+        for (Player foundPlayer : players) {
+            if (foundPlayer.getIdPlayer() == player.getIdPlayer())
+                players.remove(foundPlayer);
+        }
     }
 }
