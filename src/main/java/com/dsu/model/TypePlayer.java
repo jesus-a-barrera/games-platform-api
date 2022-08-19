06bad262f2f2ddf40b9ac375ff72dadeb229187b
Jesus Barrera <jesus.barrera@telusinternational.com>
2022-08-19 11:55:23 -0600

    Created TypePlayer model class and its CRUD funcionality

---
+++ src/main/java/com/dsu/model/TypePlayer.java
@@ -0,0 +1,60 @@
+package com.dsu.model;
+
+import java.util.List;
+
+public class TypePlayer {
+    private static List<TypePlayer> typesPlayers;
+    private int idTypePlayer;
+    private int name;
+
+    public int getIdTypePlayer() {
+        return idTypePlayer;
+    }
+
+    public void setIdTypePlayer(int idTypePlayer) {
+        this.idTypePlayer = idTypePlayer;
+    }
+
+    public int getName() {
+        return name;
+    }
+
+    public void setName(int name) {
+        this.name = name;
+    }
+
+    public static TypePlayer insertTypePlayer(TypePlayer typePlayer) {
+        typesPlayers.add(typePlayer);
+        return typePlayer;
+    }
+
+    public static TypePlayer selectTypePlayer(int id) {
+        for (TypePlayer typePlayer : typesPlayers)
+            if (typePlayer.getIdTypePlayer() == id)
+                return typePlayer;
+        return null;
+    }
+
+    public static List<TypePlayer> selectTypesPlayers() {
+        return typesPlayers;
+    }
+
+    public static TypePlayer updateTypePlayer(int id, TypePlayer typePlayer) {
+        for (TypePlayer foundTypePlayer : typesPlayers) {
+            if (foundTypePlayer.getIdTypePlayer() == id) {
+                foundTypePlayer.setIdTypePlayer(typePlayer.getIdTypePlayer());
+                foundTypePlayer.setName(typePlayer.getName());
+                return foundTypePlayer;
+            }
+        }
+        typesPlayers.add(typePlayer);
+        return typePlayer;
+    }
+
+    public static void deleteTypePlayer(TypePlayer typePlayer) {
+        for (TypePlayer foundTypePlayer : typesPlayers) {
+            if (foundTypePlayer.getIdTypePlayer() == typePlayer.getIdTypePlayer())
+                typesPlayers.remove(foundTypePlayer);
+        }
+    }
+}
