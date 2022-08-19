b2319de96ed20aa907f16bb8ccdd4f0db1bc689d
Jesus Barrera <jesus.barrera@telusinternational.com>
2022-08-19 09:51:25 -0600

    Created Player class fields

---
+++ src/main/java/com/dsu/model/Player.java
@@ -0,0 +1,26 @@
+public class Player {
+    private static List<Player> players;
+    private int idPlayer;
+    private String name;
+    private int idTypePlayer;
+
+    public static insertPlayer(Player player) {
+
+    }
+
+    public static selectPlayer(int id) {
+        
+    }
+
+    public static selectPlayers() {
+        
+    }
+
+    public static updatePlayer(int id, Player player) {
+        
+    }
+
+    public static deletePlayer(Player player) {
+        
+    }
+}
