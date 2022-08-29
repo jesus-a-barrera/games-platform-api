54e9e79175ca248004c3571d890b82cdbef54077
Gabriela <gabi16.hernandez@gmail.com>
2022-08-29 14:53:26 -0600

    List of Players controller was created

--- src/main/java/com/dsu/controller/ListOfPlayersController.java
+++ src/main/java/com/dsu/controller/ListOfPlayersController.java
@@ -8,7 +8,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/players")
 public class ListOfPlayersController {
-    
+
     @PostMapping("/")
     public Player createPlayer(Player player) {
         Player.insertPlayer(player);
