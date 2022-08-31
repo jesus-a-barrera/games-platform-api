724c86b4a8bba0716bbbdf4a68768f2601461b3c
Jesus Barrera <jesus.barrera@telusinternational.com>
2022-08-31 11:54:35 -0600

    Created tictactoc controller endopints

--- src/main/java/com/dsu/controller/ttt/TicTacToeController.java
+++ src/main/java/com/dsu/controller/ttt/TicTacToeController.java
@@ -1,6 +1,7 @@
 package com.dsu.controller.ttt;
 
 import com.dsu.model.*;
+import com.dsu.util.Dice;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -8,34 +9,70 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/ttt")
 
 public class TicTacToeController {
+    private TicTacToePlayer playerX;
+    private TicTacToePlayer playerO;
 
     @PostMapping("/")
-    public Round Start(@RequestBody Player[] players) {
-        Round round = new Round();
-        round.setPlayer1(players[0]);
-        round.setPlayer2(players[1]);
-        round.setGame(Game.selectGame(1));
-        round = Round.insertRound(round);
-        round.setBoard(Board.initBoard(round.getIdRound(), new Square[3][3]));
+    public Round start(@RequestBody Player[] players) {
+        Round round = null;
+        if (players.length == 2) {
+            round = new Round();
+            players = assignRoles(players);
+            playerX = new TicTacToePlayer(players[0], Piece.selectPiece(1));
+            playerO = new TicTacToePlayer(players[1], Piece.selectPiece(2));
+            round.setPlayer1(playerX.getPlayer());
+            round.setPlayer2(playerO.getPlayer());
+            round.setTurn(playerX.getPlayer());
+            round.setGame(Game.selectGame(1));
+            round = Round.insertRound(round);
+            round.setBoard(Board.initBoard(round.getIdRound(), new Square[3][3]));
+        }
         return round;
     }
 
     @PostMapping("/{idRound}")
-    public String MakeMove(@PathVariable int idRound, Movement movement) {
+    public Round makeMove(@PathVariable int idRound, @RequestBody Movement movement) {
         Round round = Round.selectRound(idRound);
-        if (Board.putPiece(movement.getPosition(), movement.getTicTacToePlayer().getPiece(), round.getBoard())) {
-            return "Hello";
+        if (isValidMovement(movement, round) && isCorrectTurn(movement, round) && !round.isFinished()) {
+            Position position = movement.getPosition();
+            Piece piece = movement.getTicTacToePlayer().getPiece();
+            Player player = movement.getTicTacToePlayer().getPlayer();
+            Square[][] board = round.getBoard();
+            if (Board.putPiece(position, piece, board)) {
+                if (Board.isFull() || Board.getWinner(board) != Piece.selectPiece(3)) {
+                    round.setFinished(true);
+                    if (Board.getWinner(board) != Piece.selectPiece(3))
+                        round.setWinner(player);
+                }
+                player = player.equals(playerX.getPlayer()) ? playerO.getPlayer() : playerX.getPlayer();
+                round.setTurn(player);
+                round.setBoard(board);
+            }
         }
-        return "No funciono :(";
-        //return ResponseEntity.ok().build();
+        return round;
     }
 
-    @PostMapping("/movement")
-    public Movement ViewMovement() {
-        Movement movement = new Movement(
-                new TicTacToePlayer(new Player("Juan", new TypePlayer("Bot")),
-                        Piece.selectPiece(2)),
-                new Position(1, 1));
-        return movement;
+    public boolean isValidMovement(Movement movement, Round round) {
+        TicTacToePlayer ticTacToePlayer = movement.getTicTacToePlayer();
+        Player player = ticTacToePlayer.getPlayer();
+        if ((ticTacToePlayer.equals(playerX) || ticTacToePlayer.equals(playerO))
+       && (player.equals(round.getPlayer1()) || player.equals(round.getPlayer2())))
+            return true;
+        return false;
     }
+
+    public boolean isCorrectTurn(Movement movement, Round round) {
+        Player player = movement.getTicTacToePlayer().getPlayer();
+        Player turn = round.getTurn();
+        if (player.equals(turn))
+            return true;
+        return false;
+    }
+
+    private Player[] assignRoles(Player[] gamePlayers) {
+        if (Dice.generateValue(100) > 50)
+            return new Player[] {gamePlayers[1], gamePlayers[0]};
+        return new Player[] {gamePlayers[0], gamePlayers[1]};
+    }
+
 }
